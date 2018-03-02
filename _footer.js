e7b0fed64fc2b648d9fc07571c612b524d289c50
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-03-02 14:24:01 +0100

    update to beta release

--- _footer.js
+++
@@ -1,7 +0,0 @@
-
-    window.jQuery = windowBak;
-	// Just return a value to define the module export.
-    // This example returns an object, but the module
-    // can return a function as the exported value.
-    return igv;
-}));
--- deps.js
+++
@@ -1,4 +0,0 @@
-import 'font-awesome/scss/font-awesome.scss';
-import 'jquery-ui-dist/jquery-ui';
-import 'jquery-ui-dist/jquery-ui.css';
-import './lib/igv-1.0.9.css';
